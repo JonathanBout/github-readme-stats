e6aed253993be0781a95858806ca8b01a8da236e
Jonathan Bout <jonathanbout01@gmail.com>
2024-04-09 22:07:58 +0200

    don't need that

--- express.js
+++ express.js
@@ -2,7 +2,6 @@ import "dotenv/config";
 import statsCard from "./api/index.js";
 import repoCard from "./api/pin.js";
 import langCard from "./api/top-langs.js";
-import wakatimeCard from "./api/wakatime.js";
 import gistCard from "./api/gist.js";
 import json from "./api/json/index.js";
 import express from "express";
@@ -15,6 +14,5 @@ app.listen(process.env.port || 9000, () => {
 app.get("/", statsCard);
 app.get("/pin", repoCard);
 app.get("/top-langs", langCard);
-app.get("/wakatime", wakatimeCard);
 app.get("/gist", gistCard);
 app.get("/json", json);
